4b1488eab4b4e89c146c8d730e95374bcf74439e
Manish Kumar Tiruwar <53038098+manishkumartiruwar@users.noreply.github.com>
2020-10-18 21:52:19 +0530

    Create HelloNumbers.java

---
+++ HelloNumbers.java
@@ -0,0 +1,25 @@
+public class HelloNumbers{
+   public static void main ( String args []) {
+       int Number_Of_Fingers= 10;
+       int Number_Of_Thumbs=2;
+       int NewNumber = Number_Of_Fingers + Number_Of_Thumbs;
+       
+       
+      // in java things are case sensitive which means that the two 
+      // Number and number , below are not equal.   
+       int number= 10;
+       int Number= 11;
+       
+       if ( number == Number)
+        {
+         System.out.println(" equal");
+        }
+       else {
+         System.out.prinln ("  not equal ");
+         
+         
+       }// end of void main 
+       }// end of class HelloNumbers
+       
+       
+       
