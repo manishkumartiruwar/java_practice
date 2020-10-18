339384560df40221c52f157b5ab07e74f775831d
Manish Kumar Tiruwar <53038098+manishkumartiruwar@users.noreply.github.com>
2020-10-18 21:23:28 +0530

    chapter_0001
    consists of basic java examples

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld{
+ public static void main( String args []){
+  System.out.println("Hello World "); //  this prints in the next line.
+System.out.print("Hello world");// this prints in the same line, continued where the last line was printed.
+  
+  }
+  }
