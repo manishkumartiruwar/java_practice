9d6e0a8e913d8cc704ffc575cca458d7534a30dc
Manish Kumar Tiruwar <53038098+manishkumartiruwar@users.noreply.github.com>
2020-10-20 03:11:43 +0530

    Create Primitive_Data_Types.java

---
+++ Primitive_Data_Types.java
@@ -0,0 +1,35 @@
+public class Main{
+      public static void main ( String[] args ) {
+       
+       // literal is a number or variable without expressions; 
+       /// worry  about literals and expressions
+       
+       
+       
+       
+       
+       // int has a width of 321
+       // java 8 allows _
+       int myMinValue = -2_147_483_648;
+       int myMaxValue =  2_147_483_648;
+      
+      
+      // byte has a width of 8
+      byte myByteMinValue = -128;
+      byte myByteMaxValue =  127;
+      
+      // short has a width of 16
+      short myShortMinValue =  -32768;
+      short myShortMaxValue =   32767;
+      
+      // long has a width of 64
+      // to use long put a l after the number
+      long myLongMinValue = -9_223_372_036_854_775_808L;
+      long myLongMaxValue =  9_223_372_036_854_775_807L;
+      
+      
+      //now lets 
+      
+      
+      //when you are defining a byte the number on the right  should be a byte
+      //    TYPE CASTING
