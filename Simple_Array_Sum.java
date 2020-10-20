e3be72a50804a8fb04527ceb0ca790eea6afc0cc
Manish Kumar Tiruwar <53038098+manishkumartiruwar@users.noreply.github.com>
2020-10-20 23:10:15 +0530

    Create Simple_Array_Sum.java

---
+++ Simple_Array_Sum.java
@@ -0,0 +1,28 @@
+/*
+Given an array of N integers, can you find the sum of its elements?
+  
+Input Format
+The first line cantains an integer, N , denoting the size of the array.
+The second line contains N space-seperated integers representing the array's elements.
+
+Output Format
+print the sum of the array's  elements  as a singular integer.
+
+Sample Input
+
+6
+1 2 3 4 10 11
+
+Sampler OUtput
+31
+
+
+Explanation
+we print the sum of the array's elements which is: 1+2+3+4+10+11 = 31.
+*/
+
+public class Simple_Array_Sum {
+
+
+
+}
