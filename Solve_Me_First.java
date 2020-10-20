4d6b4b7e72d14e775ce949eebd8d4352104c4678
Manish Kumar Tiruwar <53038098+manishkumartiruwar@users.noreply.github.com>
2020-10-20 22:48:18 +0530

    Create Solve_Me_First.java

---
+++ Solve_Me_First.java
@@ -0,0 +1,46 @@
+/*
+
+ complete the function Solve_Me_First to compute the sum of two integers.
+ 
+ 
+ Function prototype:
+ int Solve_Me_First(int x, int y);
+ where.
+ .x is the first integer input.
+ .y nis the second ingeger input.
+ 
+ Return values
+ .sum of the above two integers
+ 
+ Sample Input
+ x=2
+ y=3
+ 
+ Sample Output
+ 5
+ 
+ Explanation
+ The sum of the two integers X and y is computed as : 2 + 3 =5.
+ */
+ 
+ 
+ #include java.util.Scanner;
+ public class Solve_Me_First{
+         public static void main ( String args []){
+         
+         Scanner  sc = new Scanner(System.in);
+        int a;
+              a = sc.nextInt();
+        int b;
+              b = sc.nextInt();
+        int sum;
+             sum = Solve_Me_First(a, b);
+        System.out.println(sum);
+ }
+ }
+ public class int Solve_Me_First(int x, int y){
+       int sum=0;
+        sum=a+b;
+      return sum;
+ }
+         
