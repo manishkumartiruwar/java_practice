8c57e6373cc78e140908361bbaaced6cecc19838
Manish Kumar Tiruwar <53038098+manishkumartiruwar@users.noreply.github.com>
2020-10-22 06:18:04 +0530

    Update Solve_Me_First.java
    @ MANISHKUMARTIRUWAR
    1. Solving this java program led to he usage of scanner class which improved understanding of java concepts.
    2. This question has been taken from hackerrank library of algorithms.

--- Solve_Me_First.java
+++ Solve_Me_First.java
@@ -24,11 +24,20 @@
  */
  
  
- #include java.util.Scanner;
+ #include java.util.Scanner;  //  here we are including  class Scanner of java .
+
+
+
+/*The Scanner class is used to get user input, and it is found in the java.util package.
+
+To use the Scanner class, create an object of the class and use any of the available methods found in the Scanner class documentation.
+In our example, we will use the nextINT() method,which is used to read a integer  value from the user.....
+
+*/
  public class Solve_Me_First{
          public static void main ( String args []){
          
-         Scanner  sc = new Scanner(System.in) ;
+         Scanner  sc = new Scanner(System.in) ;      /// here we are creating a scanner object .
         int a ;
               a = sc.nextInt() ;
         int b;
@@ -44,3 +53,19 @@
       return sum ;
  }
          // make note of seeing scanner class in java documentation.
+
+
+/*
+Input Types
+Method	Description
+nextBoolean() used to input a boolean value from the user
+nextByte()	used to input a value from the user
+nextDouble()	used to input a value from the user
+nextFloat()	used to input a float value from the user
+nextInt()	used to input a int value from the user
+nextLine()	used to input a String value from the user
+nextLong()	used to input a long value from the user
+nextShort()	used to input a short value from the user
+*/
+
+// CREDITS hackerrank for the question.  
