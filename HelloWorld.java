73df61c7920d41e9a87a03618214138a4633b982
Manish Kumar Tiruwar <53038098+manishkumartiruwar@users.noreply.github.com>
2020-10-22 06:38:25 +0530

    Update HelloWorld.java
    @MANISHKUMARTIRUWAR 10/22/2020 @ 1:07am (UTC)
    1. Implementation of classic HelloWorld which lead to understanding of Java   PrintStream  class   
    2. Credits to java foundation.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,9 @@
+@MANISHKUMARTIRUWAR 10/22/2020 @ 1:07am (UTC)
+1. Implementation of classic HelloWorld which lead to understanding of Java   PrintStream  class   
+2. Credits to java foundation.
+ 
+
+
 public class HelloWorld{
  public static void main( String args []){
   System.out.println("Hello World "); //  this prints in the next line.
@@ -5,3 +11,17 @@ System.out.print("Hello world");// this prints in the same line, continued where
   
   }
   }
+
+/*
+Java System.out.println() is used to print an argument that is passed to it.
+The statement can be broken into 3 parts which can be understood separately as:
+
+System: It is a final class defined in the java.lang package.
+out: This is an instance of PrintStream type, which is a public and static member field of the System class.
+println(): As all instances of PrintStream class have a public method println(), hence we can invoke the same on out as well. This is an upgraded version of print(). 
+ It prints any argument passed to it and adds a new line to the output.
+ We can assume that System.out represents the Standard Output Stream.
+*/
+
+//make note of Statements System.out.println();  and System.out.print();
+//for further description refer to https://www.geeksforgeeks.org/system-out-println-in-java/
