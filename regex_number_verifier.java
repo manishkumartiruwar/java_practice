e6c2813a6dc38b56a20f6884b17338cb85945c50
Manish Kumar Tiruwar <53038098+manishkumartiruwar@users.noreply.github.com>
2020-11-03 11:27:40 +0530

    Update regex_number_verifier.java
    @manishkumartiruwar 11/03/2020 @ 5:52am (UTC)
    1. Use of regex expressions in solving the problem has led to better understanding of  optimum coding practices.

--- regex_number_verifier.java
+++ regex_number_verifier.java
@@ -1,4 +1,14 @@
-import java.util.regex.*; 
+ /*Write a Java program to read all mobile numbers present in given file and validate
+it on below criteria:
+-The first digit should contain number between 7 to 9.
+-The rest 9 digit can contain any number between 0 to 9.
+-The mobile number can have 11 digits also by including 0 at the starting.
+-The mobile number can be of 12 digits also by including 91 at the starting The
+number which satisfies the above criteria is a valid mobile Number.
+*/
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
  
   
 public class regex_number_verifier{ 
